1bc1828dac03af1c4d84da58d327f374cb588cf2
M.Furkan Uygur <76523870+mfurkanuygur@users.noreply.github.com>
2024-02-09 21:22:37 +0300

    section3 complete and tested

--- DesignCaseWithTailwind/src/components/Section2Viverra.jsx
+++ DesignCaseWithTailwind/src/components/Section2Viverra.jsx
@@ -2,8 +2,8 @@ import { threeRows } from "../Constant/constant"
 
 const Section2Viverra = () => {
     return (
-        <section className=" w-full flex justify-center items-center max-md:bg-amber-100">
-           <div className="container relative z-10 flex items-center justify-center p-20 max-md:px-4 max-md:py-12 gap-20 max-lg:flex-col ">
+        <section className="relative z-20 flex justify-center items-center max-md:bg-amber-100">
+            <div className="container flex items-center justify-center px-4 py-12 md:p-20 gap-20 max-md:gap-8 max-lg:flex-col ">
                 {threeRows.map(eachRow => (
                     <div key={eachRow.title} className="flex flex-col gap-4 max-lg:text-center max-lg:items-center max-md:justify-center">
                         <div className="relative">
@@ -14,7 +14,7 @@ const Section2Viverra = () => {
                         <p className="text-lg font-normal pr-4 max-md:pr-0">{eachRow.detail}</p>
                     </div>
                 ))}
-           </div>
+            </div>
         </section>
     )
 }
