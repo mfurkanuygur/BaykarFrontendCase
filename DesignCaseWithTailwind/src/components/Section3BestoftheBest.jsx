62b1381445784b008d63d92b90e73a872d07f0d2
M.Furkan Uygur <76523870+mfurkanuygur@users.noreply.github.com>
2024-02-09 21:34:17 +0300

    section3 complete but need to little fix

--- DesignCaseWithTailwind/src/components/Section3BestoftheBest.jsx
+++ DesignCaseWithTailwind/src/components/Section3BestoftheBest.jsx
@@ -10,9 +10,9 @@ const Section3BestoftheBest = () => {
     return (
         <section className=" bg-[#0f172a] text-white ">
             <div className="container mx-auto">
-                <div className="flex flex-col gap-20 p-20 max-lg:px-4 max-lg:py-12 mx-auto overflow-hidden">
-                    <div className="flex justify-between items-center  max-lg:flex-col max-lg:gap-8 ">
-                        <h1 className="font-extrabold text-[56px] max-lg:text-3xl ">The best of the best</h1>
+                <div className="flex flex-col gap-8 md:gap-20 p-20 max-lg:px-4 max-lg:py-12 overflow-hidden">
+                    <div className="flex justify-between items-center max-lg:flex-col max-lg:gap-8 ">
+                        <h1 className="text-[56px] font-extrabold max-lg:text-3xl ">The best of the best</h1>
                         <button className="bg-transparent border-2 rounded-lg px-6 py-5">
                             <p className="px-6 text-2xl font-bold">Sign up now</p>
                         </button>
@@ -36,13 +36,13 @@ const Section3BestoftheBest = () => {
                         <div className="relative grid grid-cols-3 max-lg:grid-cols-1 gap-20 max-md:gap-10">
                             {BestProducts.map(card => (
                                 <div key={card.id} className="bg-[#0f172a] rounded-lg  drop-shadow-[0_35px_35px_rgba(255,255,255,0.15)]  ">
-                                    <img src={card.source} alt={card.title} className="w-full rounded-t-lg" />
+                                    <img className="w-full rounded-t-lg" src={card.source} alt={card.title}/>
                                     <div className="px-8 pt-8 flex flex-col gap-4" >
                                         <h3 className="text-2xl font-bold">{card.title}</h3>
                                         <p className="text-lg font-normal">{card.text}</p>
                                     </div>
                                     <div className="p-8 flex items-center justify-center">
-                                        <button className="bg-transparent border-2 rounded-lg p-4 w-full flex justify-center  items-center gap-3" >
+                                        <button className="bg-transparent border-2 rounded-lg p-4 w-full flex justify-center items-center gap-3" >
                                             <img src={ShoppingCart} alt="shopping-cart" />
                                             {card.buttonText}
                                         </button>
