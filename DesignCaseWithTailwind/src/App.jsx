90669d5407e45a59ba597edc2f1046f0c42a136b
M.Furkan Uygur <76523870+mfurkanuygur@users.noreply.github.com>
2024-02-09 23:51:52 +0300

    app.jsx fixed

--- DesignCaseWithTailwind/src/App.jsx
+++ DesignCaseWithTailwind/src/App.jsx
@@ -1,39 +1,36 @@
-import './App.css'
-import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import Section1CollectibleSneakers from './components/Section1CollectibleSneakers'
 import Section2Viverra from './components/Section2Viverra'
 import Section3BestoftheBest from './components/Section3BestoftheBest'
 import Section4WhyJoinUs from './components/Section4WhyJoinUs'
-import mainTriangle from "./assets/images/main-triangle.png"
 import Section5Slider from './components/Section5Slider'
-import bigYellow from "./assets/images/big-yellow.png"
-import bgWaves from "./assets/images/bg-waves.png"
 import Section6Grow from './components/Section6Grow'
 import Section7Shoes from './components/Section7Shoes'
-import Deneme from './components/Deneme'
+import Footer from './components/Footer'
+
+import bgWaves from "./assets/images/bg-waves.png"
+import mainTriangle from "./assets/images/main-triangle.png"
+
 
 function App() {
-
   return (
-    <main className='  mx-auto  font-sans'>
-      <div className="relative overflow-hidden bg-gradient-to-t from-[#fffbeb] via-white to-white max-md:bg-gradient-to-t max-md:from-white max-md:to-white">
+    <main className='mx-auto font-sans'>
+      <div className="relative overflow-hidden bg-gradient-to-t from-[#fffbef] via-[#fffbed] to-[#fffbeb]  max-md:bg-gradient-to-t max-md:from-white max-md:to-white">
         <Navbar />
         <Section1CollectibleSneakers />
         <Section2Viverra />
-        <div className='absolute  bottom-0 right-0 max-md:hidden'>
-          <img className='w-svw ml-5' src={mainTriangle} alt="main-triangle" />
+        <div className='absolute bottom-0 right-0 max-md:hidden'>
+          <img className='w-svw ml-5' src={mainTriangle} alt="main-triangle"/>
         </div>
       </div>
       <Section3BestoftheBest />
       <div className='bg-gradient-to-t from-[#fffefc] via-[#fffdf8] to-[#fffcf2]'>
         <Section4WhyJoinUs />
-        {/* <Deneme/> */}
         <Section5Slider />
         <div className='relative z-10 max-md:overflow-x-hidden'>
           <Section6Grow className="relative z-20" />
           <div className='absolute bottom-0 -z-30  '>
-            <img className='md:w-svw md:h-svh max-md:max-w-fit   ' src={bgWaves} alt="" />
+            <img className='md:w-svw md:h-svh max-md:max-w-fit' src={bgWaves} alt="" />
           </div>
         </div>
       </div>
